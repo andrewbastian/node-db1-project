0b40afb6db0b10f336abd21770e05f4340748314
andrewbastian <andrew.bastian@gmail.com>
2020-02-27 18:09:30 -0800

    changed CRUD to be async

--- router/accountsRouter.js
+++ router/accountsRouter.js
@@ -4,115 +4,92 @@ const accountsDb = require('../data/dbConfig.js');
 
 
 //get all accounts
-//
-router.get('/', (req, res, next) =>{
-	accountsDb('accounts')
-		.then(accounts => {
-			res.status(200).json(accounts)
-		})
-		.catch((err) => {
-			console.log(err)
-			next(err)
-		})
+
+router.get("/", async (req, res, next) => {
+  try {
+    // translates to `SELECT * FROM accounts;`
+    const account = await accountsDb.select("*").from("accounts")
+
+    res.json(account)
+  } catch(err) {
+    next(err)
+  }
 })
+
 
 // get account by id
 
+router.get("/:id", async (req, res, next) => {
+  try {
+    // translates to `SELECT * FROM accounts WHERE id = ? LIMIT 1;`
+    // const post = await db.first("*").from("accounts").where("id", req.params.id)
+    const account = await accountsDb.first("*").from("accounts").where({ id: req.params.id })
 
-// router.get('/:id', validateAccountId, async (req, res, next) => {
-//   try {
-//   	res.json(await accountsDb.getById(req.params.id))
-//   } catch (err) {
-//   	next(err)
-//   }
-// });
-// 
-router.get('/:id', (req, res) =>{
-	accountsDb('accounts')
-		.where({id: req.params.id})
-		.first()
-		.then(account => {
-			if (account) {
-				res.status(200).json(account)
-			} else {
-				res.status(400).json({message: "no account found for that ID"})
-			}
-		})
+    res.json(account)
+  } catch(err) {
+    next(err)
+  }
 })
 
-//creat account
-//
-// im getting an an err when i send post, but when i run get all my post has gone through.
 
-router.post('/', (req, res, next) => {
-  if (validateAccount(req.body)) {
-    accountsDb('accounts')
-      .insert(req.body, 'id')
-      .then(([id]) => id)
-      .then(id => {
-        db('accounts')
-          .where({ id })
-          .first()
-          .then(account => {
-            res.status(201).json(account);
-          });
-      })
-      .catch((err) => {
-      	console.log(err)
-      	next(err)
-      });
-  } else {
-    res.status(400).json({
-      message: 'name and budget needed',
-    });
+//creat account
+
+
+router.post("/", async (req, res, next) => {
+  try {
+    const payload = {
+      name: req.body.name,
+      budget: req.body.budget,
+    }
+
+    // translates to `INSERT INTO accounts (name, budget) VALUES (?, ?);`
+    // .insert returns an array of IDs, since we could potentially insert multiple rows at once.
+    // we only want the id of the first item inserted, since we're only inserting one item.
+    const [id] = await accountsDb("accounts").insert(payload)
+    // get the newly created resource from the database in another request so we can return it.
+    const newAccount = await accountsDb("accounts").where("id", id).first()
+
+    res.status(201).json(newAccount)
+  } catch(err) {
+    next(err)
   }
-});
+})
 
 // update account
 
-router.put('/:id', (req, res, next) =>{
-	accountsDb('accounts')
-	.where({id: req.params.id})
-	.then(account => {
-		if (account) {
-			res.status(200).json({message: `${account} updated`})
-		} else {
-			res.status(400).json({message: 'no account found'})
-		}
-	})
-	.catch((err) => {
-		console.log(err)
-		next(err)
-	})
+router.put("/:id", async (req, res, next) => {
+  try {
+    const payload = {
+      name: req.body.name,
+      budget: req.body.budget,
+    }
+
+    // translates to `UPDATE accounts SET ? = ? WHERE id = ?`
+    await accountsDb("accounts").where("id", req.params.id).update(payload)
+    const account = await accountsDb("accounts").where("id", req.params.id).first()
+
+    res.json(account)
+  } catch(err) {
+    next(err)
+  }
 })
-//middleware
 
-function validateAccount({name, budget}) {
-  return name && typeof budget === 'number' && budget >= 0;
-}
 
-async function validateAccountId(req, res, next) {
-   try {
-   	const account = await accountsDb.getById(req.params.id)
+// delete account 
 
-   	if (account) {
-   		req.account = account
-   		next()
-   	} else {
-   		res.status(404).json({ message: "account not found"})
-   	}
-   } catch(err) {
-   	console.log(err)
-   	next(err)
-   }
-}
+router.delete("/:id", async (req, res, next) => {
+  try {
+    // translates to `DELETE FROM accounts WHERE id = ?`
+    // DON'T FORGET THE .WHERE OR YOU MIGHT DELETE YOUR ENTIRE TABLE
+    await accountsDb("accounts").where("id", req.params.id).del()
 
-//helper
+    res.status(204).end()
+  } catch(err) {
+    next(err)
+  }
+})
 
-function getById(id) {
-  return accountsDb('accounts')
-    .where({ id })
-    .first();
-}
+
+
 
 module.exports = router;
